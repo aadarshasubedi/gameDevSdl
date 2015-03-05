322622e073532e4d3d242918b01fbd2456b8158b
kaprikawn <wayne@kaprikawn.co.uk>
2015-03-05 10:04:40 +0000

    Create main.cpp

---
+++ chapter01/main.cpp
@@ -0,0 +1,14 @@
+#include "Game.h"
+
+int main( int argc, char* args[] ) {
+  Game* g_game = 0;
+  g_game = new Game();
+  
+  g_game->init( "Chapter 1", 100, 100, 640, 480, 0 );
+  
+  g_game->render();
+  SDL_Delay( 2000 );
+  g_game->clean();
+  
+  return 0;
+}
