94436169065a60356728f8c3d9bcd70ce8ffd8fa
Wayne Briggs <wayne@kaprikawn.co.uk>
2015-03-08 20:41:25 +0000

    adding .h

---
+++ chapter02/Game.h
@@ -0,0 +1,31 @@
+#include<SDL2/SDL.h>
+
+#ifndef GAME_H
+#define GAME_H
+
+class Game {
+  public:
+    Game(){}
+    ~Game(){}
+    
+    bool init( const char* title, int xpos, int ypos, int width, int height, int flags );
+    
+    void render();
+    
+    void clean();
+    
+    // a function to access the private running variable
+    bool running() { return m_bRunning; }
+    
+    private:
+    SDL_Window* m_pWindow;
+    SDL_Renderer* m_pRenderer;
+    
+    SDL_Texture* m_pTexture; // the new SDL_Texture variable
+    SDL_Rect m_sourceRectangle; // the first rectangle
+    SDL_Rect m_destinationRectangle; // another rectangle
+    
+    bool m_bRunning;
+};
+
+#endif //GAME_H
