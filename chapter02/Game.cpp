90e17fc4b4372b09b78aba90d64d21e85580b10d
Wayne Briggs <wayne@kaprikawn.co.uk>
2015-03-10 19:49:26 +0000

    finished chapter02

--- chapter02/Game.cpp
+++ chapter02/Game.cpp
@@ -28,11 +28,19 @@ bool Game::init( const char* title, int xpos, int ypos, int width, int height, i
   
   m_bRunning = true;
   
-  SDL_Surface* pTempSurface = SDL_LoadBMP( "rider.bmp" );
+  SDL_Surface* pTempSurface = SDL_LoadBMP( "assets/rider.bmp" );
   
   m_pTexture = SDL_CreateTextureFromSurface( m_pRenderer, pTempSurface );
   
   SDL_FreeSurface( pTempSurface );
+  
+  SDL_QueryTexture( m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h );
+  
+  m_destinationRectangle.x = m_sourceRectangle.x = 0;
+  m_destinationRectangle.y = m_sourceRectangle.y = 0;
+  m_destinationRectangle.w = m_sourceRectangle.w;
+  m_destinationRectangle.h = m_sourceRectangle.h;
+  
   
   return true;
 }
@@ -40,6 +48,9 @@ bool Game::init( const char* title, int xpos, int ypos, int width, int height, i
 void  Game::render() {
   printf( "rendering...\n" );
   SDL_RenderClear( m_pRenderer );
+  printf( "starting RenderCopy\n" );
+  SDL_RenderCopy( m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle );
+  
   SDL_RenderPresent( m_pRenderer );
 }
 
