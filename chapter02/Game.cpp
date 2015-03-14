b99a0f049d037d159c20d49d80ab26f4cacb4a01
kaprikawn <wayne@kaprikawn.co.uk>
2015-03-14 17:23:54 +0000

    Update Game.cpp

--- chapter02/Game.cpp
+++ chapter02/Game.cpp
@@ -28,13 +28,16 @@ bool Game::init( const char* title, int xpos, int ypos, int width, int height, i
   
   m_bRunning = true;
   
-  SDL_Surface* pTempSurface = SDL_LoadBMP( "assets/rider.bmp" );
+  SDL_Surface* pTempSurface = SDL_LoadBMP( "assets/animate.bmp" );
   
   m_pTexture = SDL_CreateTextureFromSurface( m_pRenderer, pTempSurface );
   
   SDL_FreeSurface( pTempSurface );
   
-  SDL_QueryTexture( m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h );
+  //SDL_QueryTexture( m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h );
+  
+  m_sourceRectangle.w = 128;
+  m_sourceRectangle.h = 82;
   
   m_destinationRectangle.x = m_sourceRectangle.x = 0;
   m_destinationRectangle.y = m_sourceRectangle.y = 0;
@@ -50,8 +53,27 @@ void  Game::render() {
   SDL_RenderClear( m_pRenderer );
   printf( "starting RenderCopy\n" );
   SDL_RenderCopy( m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle );
+  //SDL_RenderCopy( m_pRenderer, m_pTexture, 0, 0 );
   
   SDL_RenderPresent( m_pRenderer );
+}
+
+void Game::update() {
+  m_sourceRectangle.x = 128 * int( ( (  SDL_GetTicks() / 100 ) % 6 ) );
+}
+
+void Game::handleEvents() {
+  SDL_Event event;
+  if( SDL_PollEvent( &event ) ) {
+    switch( event.type ) {
+      case SDL_QUIT:
+        m_bRunning = false;
+      break;
+      
+      default:
+      break;
+    }
+  }
 }
 
 void  Game::clean() {
