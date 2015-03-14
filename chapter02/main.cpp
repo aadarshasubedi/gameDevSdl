134b88fc4c7608f1b1daa4119f248dca0e4edafd
Wayne Briggs <wayne@kaprikawn.co.uk>
2015-03-14 15:30:36 +0000

    updating main

--- chapter02/main.cpp
+++ chapter02/main.cpp
@@ -1,3 +1,14 @@
-int main() {
+#include "Game.h"
+
+int main( int argc, char* args[] ) {
+  Game* g_game = 0;
+  g_game = new Game();
+  
+  g_game->init( "Chapter 1", 100, 100, 640, 480, 0 );
+  
+  g_game->render();
+  SDL_Delay( 5000 );
+  g_game->clean();
+  
   return 0;
 }
