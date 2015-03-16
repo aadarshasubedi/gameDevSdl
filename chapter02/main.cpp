db728a4dfb2435f4e8b5e16a68db1c6a7d073396
kaprikawn <wayne@kaprikawn.co.uk>
2015-03-16 09:12:43 +0000

    Update main.cpp

--- chapter02/main.cpp
+++ chapter02/main.cpp
@@ -4,7 +4,7 @@ int main( int argc, char* args[] ) {
   Game* g_game = 0;
   g_game = new Game();
   
-  g_game->init( "Chapter 1", 100, 100, 640, 480, 0 );
+  g_game -> init( "Chapter 1", 100, 100, 640, 480, 0 );
   
   while ( g_game -> running() ) {
     g_game -> handleEvents();
