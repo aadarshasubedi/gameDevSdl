335433597c53b070c64f4b1c5ee0a6878b63ef44
kaprikawn <wayne@kaprikawn.co.uk>
2015-03-17 12:23:40 +0000

    Create TextureManager.h

---
+++ chapter02/TextureManager.h
@@ -0,0 +1,17 @@
+#ifndef TEXTURE_MANAGER_H
+#define TEXTURE_MANAGER_H
+
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_image.h>
+
+bool load( std::string filename, std::string id, SDL_Renderer* rRenderer );
+
+void draw( std::string id, int x, int y, int width, int height, SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE );
+
+// drawframe
+
+void drawFrame( std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE );
+
+std::map<std::string, SDL_Texture*> m_textureMap;
+
+#endif //TEXTURE_MANAGER_H
