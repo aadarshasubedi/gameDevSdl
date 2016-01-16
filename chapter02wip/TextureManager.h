1cc43f6a30808a1286231f24f8718455980b3f64
kaprikawn <wayne@kaprikawn.co.uk>
2016-01-16 18:12:36 +0000

    Changes from code example

--- chapter02wip/TextureManager.h
+++ chapter02wip/TextureManager.h
@@ -1,30 +1,46 @@
 #ifndef TEXTURE_MANAGER_H
 #define TEXTURE_MANAGER_H
 
-#include<SDL2/SDL.h>
-#include<SDL2/SDL_image.h>
-
 #include <string.h>
 #include <iostream>
 #include <map>
 
+#include<SDL2/SDL.h>
+
 class TextureManager {
-  private:
-    TextureManager(){}
-    //~TextureManager(){}
-    
   public:
+    static TextureManager* Instance() {
+      if( s_pInstance == 0 ) {
+        s_pInstance = new TextureManager();
+        return s_pInstance;
+      }
+      return s_pInstance;
+    }
     
-    bool load( std::string filename, std::string id, SDL_Renderer* rRenderer );
+    bool load( std::string fileName, std::string id, SDL_Renderer* rRenderer );
     
-    void draw( std::string id, int x, int y, int width, int height, SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE );
+    void clearTextureMap();
+    void clearFromTextureMap( std::string id);
     
-    // drawframe
-    void drawFrame( std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE );
+    void draw(std::string id, int x, int y, int width, int height, SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+    void drawFrame(std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer* pRenderer, double angle, int alpha, SDL_RendererFlip flip = SDL_FLIP_NONE);
+    void drawTile(std::string id, int margin, int spacing, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer *pRenderer);
+    
+    std::map<std::string, SDL_Texture*> getTextureMap() { return m_textureMap; }
+    
+  private:
+    TextureManager() {}
+    ~TextureManager() {}
+    
+    TextureManager( const TextureManager& );
+    TextureManager& operator=( const TextureManager& );
     
     std::map<std::string, SDL_Texture*> m_textureMap;
     
+    static TextureManager* s_pInstance;
+    
 };
 
+typedef TextureManager TheTextureManager;
 
 #endif //TEXTURE_MANAGER_H
