b094674d4af78cbbfb5cbecb740d84796e321ffb
kaprikawn <wayne@kaprikawn.co.uk>
2016-01-16 17:53:44 +0000

    Update TextureManager.h

--- chapter02wip/TextureManager.h
+++ chapter02wip/TextureManager.h
@@ -9,19 +9,20 @@
 #include <map>
 
 class TextureManager {
-  public:
-    //TextureManager(){}
+  private:
+    TextureManager(){}
     //~TextureManager(){}
     
-  bool load( std::string filename, std::string id, SDL_Renderer* rRenderer );
-
-  void draw( std::string id, int x, int y, int width, int height, SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE );
-
-  // drawframe
-
-  void drawFrame( std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE );
-
-  std::map<std::string, SDL_Texture*> m_textureMap;
+  public:
+    
+    bool load( std::string filename, std::string id, SDL_Renderer* rRenderer );
+    
+    void draw( std::string id, int x, int y, int width, int height, SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE );
+    
+    // drawframe
+    void drawFrame( std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE );
+    
+    std::map<std::string, SDL_Texture*> m_textureMap;
     
 };
 
