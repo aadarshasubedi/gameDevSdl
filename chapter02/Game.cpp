924675bddef25877f1ca45741d1e8b6a717ca2f6
kaprikawn <wayne@kaprikawn.co.uk>
2016-01-30 15:21:09 +0000

    Update Game.cpp

--- chapter02/Game.cpp
+++ chapter02/Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include "TextureManager.h"
 
 bool Game::init( const char* title, int xpos, int ypos, int width, int height, bool fullscreen ) {
 
@@ -34,23 +35,25 @@ bool Game::init( const char* title, int xpos, int ypos, int width, int height, b
   
   m_bRunning = true;
   
+  m_textureManager.load( "assets/animate-alpha.png", "animate", m_pRenderer );
+  
   //SDL_Surface* pTempSurface = SDL_LoadBMP( "assets/animate.bmp" );
   //SDL_Surface* pTempSurface = IMG_Load( "assets/animate.png" );
-  SDL_Surface* pTempSurface = IMG_Load( "assets/animate-alpha.png" );
+  //SDL_Surface* pTempSurface = IMG_Load( "assets/animate-alpha.png" );
   
-  m_pTexture = SDL_CreateTextureFromSurface( m_pRenderer, pTempSurface );
+  //m_pTexture = SDL_CreateTextureFromSurface( m_pRenderer, pTempSurface );
   
-  SDL_FreeSurface( pTempSurface );
+  //SDL_FreeSurface( pTempSurface );
   
   //SDL_QueryTexture( m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h );
   
-  m_sourceRectangle.w = 128;
-  m_sourceRectangle.h = 82;
+  //m_sourceRectangle.w = 128;
+  //m_sourceRectangle.h = 82;
   
-  m_destinationRectangle.x = m_sourceRectangle.x = 0;
-  m_destinationRectangle.y = m_sourceRectangle.y = 0;
-  m_destinationRectangle.w = m_sourceRectangle.w;
-  m_destinationRectangle.h = m_sourceRectangle.h;
+  //m_destinationRectangle.x = m_sourceRectangle.x = 0;
+  //m_destinationRectangle.y = m_sourceRectangle.y = 0;
+  //m_destinationRectangle.w = m_sourceRectangle.w;
+  //m_destinationRectangle.h = m_sourceRectangle.h;
   
   
   return true;
@@ -71,15 +74,20 @@ void Game::handleEvents() {
 }
 
 void Game::update() {
-  m_sourceRectangle.x = 128 * int( ( (  SDL_GetTicks() / 100 ) % 6 ) );
+  //m_sourceRectangle.x = 128 * int( ( (  SDL_GetTicks() / 100 ) % 6 ) );
+  m_currentFrame = int( ( ( SDL_GetTicks() / 100 ) % 6 ) );
 }
 
 void  Game::render() {
   //printf( "rendering...\n" );
   SDL_RenderClear( m_pRenderer );
+  
+  m_textureManager.draw( "animate", 0, 0, 128, 82, m_pRenderer );
+  m_textureManager.drawFrame( "animate", 100, 100, 128, 82, 1, m_currentFrame, m_pRenderer );
+  
   //printf( "starting RenderCopy\n" );
   //SDL_RenderCopy( m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle );
-  SDL_RenderCopyEx( m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle, 0, 0, SDL_FLIP_HORIZONTAL );
+  //SDL_RenderCopyEx( m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle, 0, 0, SDL_FLIP_HORIZONTAL );
   //SDL_RenderCopy( m_pRenderer, m_pTexture, 0, 0 );
   
   SDL_RenderPresent( m_pRenderer );
