84ed31c1c3e594f78baa1f7c0e6eae32db96e12e
kaprikawn <wayne@kaprikawn.co.uk>
2016-02-23 17:40:51 +0000

    Update GameObject.h

--- chapter03/GameObject.h
+++ chapter03/GameObject.h
@@ -17,7 +17,6 @@ class GameObject {
     
     int m_width;
     int m_height;
-    std::vector<GameObject*>  m_gameObjects;
 
   public:
     virtual void load( int x, int y, int width, int height, std::string textureID ) {
@@ -34,17 +33,14 @@ class GameObject {
       
     }
     
-    void draw( SDL_Renderer* pRenderer ) {
-      for( std::vector<GameObject*>::size_type i = 0; i != m_gameObjects.size(); i++ ) {
-        m_gameObjects[i] -> draw( m_pRenderer );
-        
-      }
-      //TextureManager::Instance() -> drawFrame( m_textureID, m_x, m_y, m_width, m_height, m_currentRow, m_currentFrame, pRenderer, SDL_FLIP_NONE );
-    }
-    
     void update() {
       m_x += 1;
     }
+    
+    void draw( SDL_Renderer* pRenderer ) {
+      TextureManager::Instance() -> drawFrame( m_textureID, m_x, m_y, m_width, m_height, m_currentRow, m_currentFrame, pRenderer, SDL_FLIP_NONE );
+    }
+    
     void clean() {}
     
 };
